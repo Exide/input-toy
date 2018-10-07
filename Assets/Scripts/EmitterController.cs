b0251146c4f125d9a5d1a35ae97c754be0b3bd29
Exide <exide@hotmail.com>
2018-10-07 15:54:45 -0700

    Change particle color with scroll wheel.

--- Assets/Scripts/EmitterController.cs
+++ Assets/Scripts/EmitterController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EmitterController : MonoBehaviour {
 	
@@ -10,6 +10,11 @@ public class EmitterController : MonoBehaviour {
 
 	private void Update () {
 		if (!gameObject.activeSelf) return;
+		updateParticlePosition();
+		updateParticleColor();
+	}
+
+	private void updateParticlePosition() {
 		if (Camera.main == null) throw new NoCameraException();
 		var mousePosition = Input.mousePosition;
 		mousePosition.x = Mathf.Clamp(mousePosition.x, 10, Screen.width - 10);
@@ -18,4 +23,27 @@ public class EmitterController : MonoBehaviour {
 		transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
 	}
 	
+	private void updateParticleColor() {
+		var mouseWheelDelta = Input.GetAxis("Mouse ScrollWheel");
+		if (mouseWheelDelta == 0) return;
+		var particleSettings = GetComponent<ParticleSystem>().main;
+		particleSettings.startColor = modifyHue(particleSettings.startColor.color, mouseWheelDelta);
+	}
+
+	private Color modifyHue(Color color, float change) {
+		float hue;
+		float saturation;
+		float value;
+		Color.RGBToHSV(color, out hue, out saturation, out value);
+		hue += change;
+		hue = loopOnBoundary(hue, 0, 1);
+		return Color.HSVToRGB(hue, saturation, value);
+	}
+
+	private float loopOnBoundary(float value, float minimum, float maximum) {
+		var delta = maximum - minimum;
+		if (value > maximum) return value - delta;
+		if (value < minimum) return value + delta;
+		return value;
+	}
 }
