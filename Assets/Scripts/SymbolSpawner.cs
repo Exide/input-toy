8f5b7af176ba06f7948c0a7a33e7fd1721eb07b1
Exide <exide@hotmail.com>
2018-10-07 14:51:24 -0700

    Small tweaks to symbol placement.

--- Assets/Scripts/SymbolSpawner.cs
+++ Assets/Scripts/SymbolSpawner.cs
@@ -22,9 +22,9 @@ public class SymbolSpawner : MonoBehaviour {
 
     private Vector3 getRandomVisiblePosition() {
         if (Camera.main == null) throw new NoCameraException();
-        var x = Random.Range(0, Screen.width);
-        var y = Random.Range(0, Screen.height);
-        var z = Camera.main.farClipPlane / 2;
+        var x = Random.Range(100, Screen.width - 100);
+        var y = Random.Range(100, Screen.height - 100);
+        var z = Camera.main.farClipPlane - 1;
         var screenPosition = new Vector3(x, y, z);
         return Camera.main.ScreenToWorldPoint(screenPosition);
     }
