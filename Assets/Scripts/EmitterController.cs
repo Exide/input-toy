8e936c86c26d01725815e1e63d49c3fea273fe21
Exide <exide@hotmail.com>
2018-10-07 14:55:13 -0700

    Emit particles at the mouse cursor if a mouse is detected.

---
+++ Assets/Scripts/EmitterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EmitterController : MonoBehaviour {
+	
+	private void Start () {
+		if (!Input.mousePresent) {
+			gameObject.SetActive(false);
+		}
+	}
+
+	private void Update () {
+		if (!gameObject.activeSelf) return;
+		if (Camera.main == null) throw new NoCameraException();
+		var mousePosition = Input.mousePosition;
+		mousePosition.z = Camera.main.farClipPlane - 1;
+		transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+	}
+	
+}
